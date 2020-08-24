c0fd1970eab14ef287275685f7434e186e686487
Charlotte Merzbacher <cjmerzbacher@gmail.com>
2020-08-24 10:52:22 -0700

    Added logistic regression

--- regression/regression_data_cleaning.py
+++ regression/regression_data_cleaning.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression
-
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.model_selection import train_test_split
 #Load data
 fires = pd.read_csv('FTM_fires.csv')
 fires.head()
@@ -22,19 +22,23 @@ species = np.array(data.Species).reshape((-1, 1))
 clspercent = np.array(data.CLS_percent).reshape((-1, 1))
 burned = np.array(data.yr0status).reshape((-1, 1))
 
-burned = clspercent
-clspercent = species
+cls_train, cls_test, burn_train, burn_test = train_test_split(clspercent, burned, test_size=0.2, random_state=42)
 
-model = LinearRegression()
-model.fit(clspercent, burned)
-R_sq = model.score(clspercent, burned)
-pred_burned = model.predict(clspercent)
 
-print('Slope of regression line: ', model.coef_[0])
-print('R-squared coefficient: ', R_sq)
+model = LogisticRegression(solver='liblinear', random_state=0)
+model.fit(cls_train, burn_train)
+
+prob_burned = model.predict_proba(burn_test)
+pred_burned = model.predict(burn_test)
+
 
 #Plot data points and linreg line
 fig, ax = plt.subplots(figsize = (9, 5))
-plt.plot(data.CLS_percent, data.yr0status, 'k.')
-plt.plot(clspercent, pred_burned, 'b-')
+plt.plot(cls_train, cls_test, '.', alpha=0.5)
 
+#plt.plot(pred_burned, clspercent, '-')
+
+#R_sq = model.score(burned, clspercent)
+#pred_burned = model.predict(burned)
+print('Slope of regression line: ', model.coef_[0])
+print('R-squared coefficient: ', R_sq
