48b5ca21a2659187c30ca71e9ed8acb7f5ff0e1a
Charlotte Merzbacher <cjmerzbacher@gmail.com>
2020-08-29 11:27:12 -0700

    Analyze tree height data

--- regression/regression_data_cleaning.py
+++ regression/regression_data_cleaning.py
@@ -1,44 +1,68 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.linear_model import LinearRegression, LogisticRegression
-from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from mpl_toolkits import mplot3d
+
 #Load data
-fires = pd.read_csv('FTM_fires.csv')
-fires.head()
+data = pd.read_csv('vegstr.csv')
 
-trees = pd.read_csv('FTM_trees.csv')
-trees.head()
-
-#Merge trees and fire data so each row is a tree with its associated fire
-data = pd.merge(trees, fires, on='YrFireName', how='inner')
-
-#Clean data
-data = data.drop(columns=['Unnamed: 16', 'Unnamed: 17', 'Unnamed: 18' ,'Unnamed: 19'])
-data = data.dropna(subset=['CLS_percent', 'yr0status']) #Remove trees whose status was not assessed directly after fire
+#GOAL: predict tree height based on canopy diameter, etc.
 
 #Univariate linear regression
-species = np.array(data.Species).reshape((-1, 1))
-clspercent = np.array(data.CLS_percent).reshape((-1, 1))
-burned = np.array(data.yr0status).reshape((-1, 1))
+height = np.array(data.stemheight).reshape((-1, 1))
+canopydiam = np.array(data.maxcanopydiam).reshape((-1, 1))
 
-cls_train, cls_test, burn_train, burn_test = train_test_split(clspercent, burned, test_size=0.2, random_state=42)
+model = LinearRegression()
+model.fit(canopydiam, height)
+R_sq = model.score(canopydiam, height)
+pred_height = model.predict(canopydiam)
 
-
-model = LogisticRegression(solver='liblinear', random_state=0)
-model.fit(cls_train, burn_train)
-
-prob_burned = model.predict_proba(burn_test)
-pred_burned = model.predict(burn_test)
-
-
-#Plot data points and linreg line
-fig, ax = plt.subplots(figsize = (9, 5))
-plt.plot(cls_train, cls_test, '.', alpha=0.5)
-
-#plt.plot(pred_burned, clspercent, '-')
-
-#R_sq = model.score(burned, clspercent)
-#pred_burned = model.predict(burned)
 print('Slope of regression line: ', model.coef_[0])
-print('R-squared coefficient: ', R_sq
+print('R-squared coefficient: ', R_sq)
+
+fig, ax = plt.subplots(figsize = (9, 5))
+plt.plot(data.maxcanopydiam, data.stemheight, 'k.', alpha=0.5, label='Data points')
+plt.plot(canopydiam, pred_height, '-', label='Regression line')
+plt.legend()
+plt.grid()
+ax.set_xlabel('Canopy diameter, m')
+ax.set_ylabel('Trunk height, m')
+
+#Multivariate regression
+data = data.dropna(subset=['dbh']).reset_index(drop=True)
+X = data[['maxcanopydiam', 'dbh']]
+height = data['stemheight']
+model = LinearRegression()
+model.fit(X, height)
+R_sq = model.score(X, height)
+pred_height = model.predict(X)
+
+print('Slope of regression line: ', model.coef_[0])
+print('R-squared coefficient: ', R_sq)
+
+
+fig = plt.figure(figsize = (9,6))
+ax = plt.axes(projection='3d')
+ax.scatter3D(data.dbh, data.maxcanopydiam, data.stemheight, color='black')
+ax.plot_trisurf(X.dbh, X.maxcanopydiam, pred_height, alpha=0.5)
+ax.set_xlabel('Trunk diameter, cm')
+ax.set_ylabel('Canopy diameter, m')
+ax.set_zlabel('Trunk height, m')
+plt.legend()
+
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(9, 4.5), tight_layout=True)
+ax1.plot(data.maxcanopydiam, data.stemheight, 'k.', alpha=0.5, label='Data points')
+ax1.plot(X.maxcanopydiam, pred_height, '.', label='Predicted Values')
+ax1.legend()
+ax1.grid()
+ax1.set_xlabel('Canopy diameter, m')
+ax1.set_ylabel('Trunk height, m')
+
+ax2.plot(data.dbh, data.stemheight, 'k.', alpha=0.5, label='Data points')
+ax2.plot(X.dbh, pred_height, '.', label='Predicted Values')
+ax2.legend()
+ax2.grid()
+ax2.set_xlabel('Trunk diameter, cm')
+ax2.set_ylabel('Trunk height, m')
